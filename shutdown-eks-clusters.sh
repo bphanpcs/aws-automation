85e1ed19f60959f4afaadc10d8a14b248b06f41a
Binh Phan <92009631+bphanpcs@users.noreply.github.com>
2023-04-05 09:40:31 -0700

    Create shutdown-eks-clusters.sh
    Script that performs the following 
    1. Logs in to your AWS account using pre-configured credentials stored locally and shuts down all EKS clusters using eksctl, follow these steps:
    
    Ensure that you have the AWS CLI and eksctl installed on your local machine. If not, install them by following the respective official documentation:
    
    AWS CLI: https://aws.amazon.com/cli/
    eksctl: https://eksctl.io/introduction/#installation
    Configure your AWS credentials locally by running the aws configure command and following the prompts. For more information, refer to the AWS CLI Configuration guide: https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-quickstart.html
    
    Ensure script is executable with chmod +x shutdown-eks-cluster.sh
    Execute with 'bash shutdown-eks-clusters.sh'

---
+++ shutdown-eks-clusters.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# Check if AWS CLI and eksctl are installed
+if ! command -v aws &> /dev/null || ! command -v eksctl &> /dev/null; then
+    echo "ERROR: AWS CLI and/or eksctl are not installed. Please install them and try again."
+    exit 1
+fi
+
+# Login to AWS account using pre-configured credentials
+echo "Logging in to AWS account..."
+aws sts get-caller-identity
+
+# Get a list of all AWS regions
+echo "Getting the list of AWS regions..."
+regions=$(aws ec2 describe-regions --query 'Regions[].RegionName' --output text)
+
+# Initialize an empty string for storing cluster information
+clusters=""
+
+# Iterate through all regions and get a list of EKS clusters in each region
+for region in $regions; do
+    echo "Checking for EKS clusters in region: $region"
+    regional_clusters=$(aws eks list-clusters --region "$region" --query 'clusters' --output text)
+
+    if [ -n "$regional_clusters" ]; then
+        clusters="$clusters $regional_clusters"
+        for cluster in $regional_clusters; do
+            echo "Found EKS cluster: $cluster in region: $region"
+        done
+    fi
+done
+
+if [ -z "$clusters" ]; then
+    echo "No EKS clusters found in your AWS account."
+    exit 0
+fi
+
+# Shutdown all EKS clusters
+echo "Shutting down EKS clusters..."
+for region in $regions; do
+    regional_clusters=$(aws eks list-clusters --region "$region" --query 'clusters' --output text)
+    for cluster in $regional_clusters; do
+        echo "Deleting EKS cluster: $cluster in region: $region"
+        eksctl delete cluster --name "$cluster" --region "$region" --wait
+    done
+done
+
+echo "All EKS clusters have been shut down."
